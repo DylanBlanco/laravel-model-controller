4a9bdd0b83d609fdaa32cea76f636dfe95ee90b9
DylanBlanco <dylanblanco1198@gmail.com>
2024-10-22 16:33:11 +0200

    app.http.guest / creato MovieController

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,8 @@
 <?php
-
+use App\Http\Controllers\Guest\MovieController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    $firstName = 'Gino';
-    $lastName = 'Paoli';
-
-    /*
-        compact: crea un array associativo le cui chiavi sono le stringhe
-                 che mettiamo tra le parentesi, mentre i valori di tali
-                 chiavi sono i valori delle variabili con i nomi corrispondenti
-                 alle stringhe inserite
-
-        compact('firstName', 'lastName')
-         |                                     |
-         V                                     V
-
-         [
-            'firstName' => $firstName,
-            'lastName' => $lastName,
-         ]
-    */
-
-    /*
-        dd: vuol dire dump and die, cioè fai il var_dump (più carino però)
-            e poi stoppa l'esecuzione
-    */
-    // dd(compact('firstName', 'lastName'));
-
-    return view('welcome', [
-        'firstName' => $firstName,
-        'lastName' => $lastName,
-    ]);
-    // return view('welcome', compact('firstName', 'lastName'));
-});
+Route::get('/', [MovieController::class, 'index']);
 
 Route::get('/chi-siamo', function () {
     return view('subpages.about');
